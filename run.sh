af6914e651d7d57205d0a76bcba9055bc9f7af25
arnydo <arnydo@gmail.com>
2018-10-29 08:51:32 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-MISP_URL=http://misp.thevillages.com
-MISP_KEY=12312312312kj1h2kj1h2kjh123123
+MISP_URL=""
+MISP_KEY=""
 
 sed -i -e '/s/YOUR_MISP_URL/${MISP_URL}/g' /root/mail_to_misp/mail_to_misp_config.py
 sed -i -e '/s/YOUR_KEY_HERE/${MISP_KEY}/g' /root/mail_to_misp/mail_to_misp_config.py
